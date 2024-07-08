f679cb9630377777e9fbc4e72b45e3675abb574d
Marina Watowich <49698282+mwatowich@users.noreply.github.com>
2024-07-08 10:14:48 -0500

    Update lifestyleScale_subsistence.R

--- scripts/lifestyleScale_subsistence.R
+++ scripts/lifestyleScale_subsistence.R
@@ -14,69 +14,39 @@ library(dplyr)
 data_turkana <- data_turkana %>%
   mutate(subsistence = case_when(
     main_subsistence_activity %in% c('Animal Keeping', 'Herding', 'Gathering', 'Farming', 
-                                     'Fishing', 'Other Subsistence', 'Hunting and Gathering') ~ 1,
-    main_subsistence_activity %in% c('Formal Employment') ~ 3,
+                                     'Fishing', 'Other Subsistence', 'Hunting and Gathering', 
+                                     'Hunting and Gathering, Animal Keeping') ~ 0,
+    main_subsistence_activity %in% c('Formal Employment') ~ 1,
     is.na(main_subsistence_activity) ~ NA_real_,
-    TRUE ~ 2
+    TRUE ~ 0.5
   ))
 
 
 # Orang Asli
-
-# Recode rank columns
-# currently rank is: 1=most, 2=second most, 3=least, 0=never
-# recode ranks to: 0=never, 1=least, 2=second most, 3=most
 data_orangAsli <- data_orangAsli %>% 
   mutate(wage_rank_recode = case_when(
-    wage_rank == "1" ~ 3,
-    wage_rank == "2" ~ 2,
-    wage_rank == "3" ~ 1,
-    wage_rank == "0" ~ 0,
-    TRUE ~ NA_real_
-  ),
-  forage_rank_recode = case_when(
-    forage_rank == "1" ~ 3,
-    forage_rank == "2" ~ 2,
-    forage_rank == "3" ~ 1,
-    forage_rank == "0" ~ 0,
-    TRUE ~ NA_real_
-  ),
-  crop_rank_recode = case_when(
-    crop_rank == "1" ~ 3,
-    crop_rank == "2" ~ 2,
-    crop_rank == "3" ~ 1,
-    crop_rank == "0" ~ 0,
-    TRUE ~ NA_real_
-  ),
-  subsistence = ((case_when(
-    # wage: higher rank = more urban/wage labor 
-    wage_rank_recode == 3 ~ 3/3, 
-    wage_rank_recode == 2 ~ 2/3,
-    wage_rank_recode == 1 ~ 1/3,
-    wage_rank_recode == 0 ~ 0/3,
-    TRUE ~ NA_real_
-  )) +
-    (case_when(
-      # crop: higher rank = more urban 
-      crop_rank_recode == 3 ~ 3/3, 
-      crop_rank_recode == 2 ~ 2/3,
-      crop_rank_recode == 1 ~ 1/3,
-      crop_rank_recode == 0 ~ 0/3,
-      TRUE ~ NA_real_
-    )) + 
-    (case_when(
-      # forage: higher rank = least urban, thus code as 0 (lower = less urban)
-      forage_rank_recode == 3 ~ 0/3, 
-      forage_rank_recode == 2 ~ 1/3,
-      forage_rank_recode == 1 ~ 2/3,
-      forage_rank_recode == 0 ~ 3/3,
-      TRUE ~ NA_real_
-    ))) +
-    # frequency of hunting, fishing, or gathering in past week 
-    (hunted_1wk/7) + 
-    (fished_1wk/7) + 
-    (gather_1wk/7) + 
-    # Did you participate in wage labor in past month? Yes (1) / no (0) 
-    wage_past_month
-  )
+    wage_rank == "1" ~ 3/3, # 1 = most. Recode to 3/3: higher wage rank = more urban
+    wage_rank == "2" ~ 2/3,
+    wage_rank == "3" ~ 1/3,
+    wage_rank == "0" ~ 0/3,
+    TRUE ~ NA_real_),
+    crop_rank_recode = case_when(
+      crop_rank == "1" ~ 3/3, # 1 = most. Recode to 3/3: higher crop rank = more urban 
+      crop_rank == "2" ~ 2/3,
+      crop_rank == "3" ~ 1/3,
+      crop_rank == "0" ~ 0/3,
+      TRUE ~ NA_real_),
+    forage_rank_recode = case_when(
+      forage_rank == "1" ~ 0/3, # 1 = most reliance on foraging. Recode to 0/3: higher forage rank = least urban
+      forage_rank == "2" ~ 1/3,
+      forage_rank == "3" ~ 2/3,
+      forage_rank == "0" ~ 3/3,
+      TRUE ~ NA_real_)) %>% 
+  rowwise() %>%
+  mutate(subsistence = (sum(c(wage_rank_recode, crop_rank_recode, forage_rank_recode, 
+                              1-hunted_1wk/7, 1-fished_1wk/7, 1-gather_1wk/7, wage_past_month), na.rm = TRUE))/
+           (sum(!is.na(c(wage_rank_recode, crop_rank_recode, forage_rank_recode, 
+                         hunted_1wk, fished_1wk, gather_1wk, wage_past_month))))) %>%
+  ungroup()
 
+data_orangAsli[is.na(data_orangAsli$subsistence),]$subsistence <- NA
