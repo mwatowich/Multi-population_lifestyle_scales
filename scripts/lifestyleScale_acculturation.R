df8b973fe0baded79fe5f967b52a756e0229605a
Marina Watowich <49698282+mwatowich@users.noreply.github.com>
2024-07-08 10:24:01 -0500

    Update lifestyleScale_acculturation.R

--- scripts/lifestyleScale_acculturation.R
+++ scripts/lifestyleScale_acculturation.R
@@ -17,14 +17,14 @@ data_turkana <- data_turkana %>%
     grepl("Primary|primary", highest_education_level) ~ 1/3,
     grepl("Secondary|secondary", highest_education_level) ~ 2/3,
     grepl("Tertiary|tertiary", highest_education_level) ~ 3/3,
-    TRUE ~ NA_real_
+    .default = NA
   ), 
   # Languages the participant speaks 
   language_score = case_when(
     grepl("English", languages_you_speak) ~ 2/2,
     grepl("Swahili", languages_you_speak) ~ 1/2,
     grepl("Turkana", languages_you_speak) ~ 0,
-    TRUE ~ NA_real_
+    .default = NA
   )) %>% 
   rowwise() %>% 
   # Calculate score on variables with complete data 
@@ -42,19 +42,19 @@ data_orangAsli <- data_orangAsli %>%
     highest_education_stage == 2 ~ 2/3,
     highest_education_stage == 1 ~ 2/3,
     highest_education_stage == 0 ~ 0/3,
-    TRUE ~ NA_real_
+    .default = NA
   ), 
   # Type of religion participant primarily practices 
   religion_num = case_when(
     religion___islam == 1 | religion___bahai == 1 | religion___budd == 1 | religion___christ == 1 | religion___hindu == 1 ~ 1,
     religion___oa == 1 ~ 0,
-    TRUE ~ NA_real_
+    .default = NA
   ), 
   # If given the choice between these, which meat would you choose? 
   choose_meat_score = case_when(
     choose_meat == "beef" ~ 1,
     choose_meat == "monkey" ~ 0,
-    TRUE ~ NA_real_
+    .default = NA
   )) %>% 
   rowwise() %>%
   mutate(acculturation_score = (sum(c(highest_education_stage_num, religion_num, mysia_pres_yn, language/3, choose_meat_score), na.rm = TRUE))/
