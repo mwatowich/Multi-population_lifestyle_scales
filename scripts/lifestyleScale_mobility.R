25ef981b0ae9388dc39ba47c9a193664f6b65523
Marina Watowich <49698282+mwatowich@users.noreply.github.com>
2024-07-08 10:16:26 -0500

    Update lifestyleScale_mobility.R

--- scripts/lifestyleScale_mobility.R
+++ scripts/lifestyleScale_mobility.R
@@ -19,5 +19,8 @@ data_orangAsli <- data_orangAsli %>%
                                   ),
          kl_times_numeric = case_when(kl_times > 5 ~ 1, 
                                       kl_times > 0 & kl_times <= 5 ~ 0.5, 
-                                      kl_times == 0 ~ 0, .default = NA),
-         mobility = town_2wk_new + kl_times_numeric + hh_item___moto + hh_item___car)
+                                      kl_times == 0 ~ 0, .default = NA)) %>% 
+  rowwise() %>%
+  mutate(mobility = (sum(c(town_2wk_new, kl_times_numeric, hh_item___moto, hh_item___car), na.rm = TRUE))/
+           (sum(!is.na(c(town_2wk_new, kl_times_numeric, hh_item___moto, hh_item___car))))) %>%
+  ungroup()
